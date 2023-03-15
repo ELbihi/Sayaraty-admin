24778b893a9ffb2b7492b38624b09a416578852b
ELbihi <bbihi115@gmail.com>
2023-03-15 15:53:13 +0100

    heelo

---
+++ a.js
@@ -0,0 +1,14 @@
+/*let vMenu = document.getElementsByClassName("sub-menu-wrap");
+   function toggle(){
+vMenu.cclassList.toggle("open");    
+}
+vMenu.onclick=toggle();
+/*const dropp= document.querySelector(".drop"),
+btn=dropp.querySelector(".sub-menu-wrap");
+
+dropp.addEventListener("click",()=> dropp.classList.toggle(open));*/
+var nu = document.getElementsById("u");
+
+function togglle(){
+    nu.classList.toggle("open")
+}
